311057e5aa690c43385d781efc83c1ff9ff9ee04
shubham <srkale856@gmail.com>
2024-05-19 09:20:47 +0530

    selenium test added by person X

---
+++ src/test/java/stepDefination/SeleniumTest.java
@@ -0,0 +1,15 @@
+package stepDefination;
+
+public class SeleniumTest 
+{
+
+	public void Test1()
+	{
+		System.out.println("this is the Test1 added by person x");
+	}
+	
+	public void Test2()
+	{
+		System.out.println("this is the test2 added by person X");
+	}
+}
