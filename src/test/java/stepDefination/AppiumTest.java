e0ab59f81a8daf9a3dd8ae45f2b242a0b8878f9f
shubham <srkale856@gmail.com>
2024-05-19 09:35:46 +0530

    appium test added by person Y

---
+++ src/test/java/stepDefination/AppiumTest.java
@@ -0,0 +1,15 @@
+package stepDefination;
+
+public class AppiumTest 
+{
+	public void Test3()
+	{
+		System.out.println("this is test 3 added by the person Y");
+	}
+	
+	public void Test4()
+	{
+		System.out.println("this is test 4 added by person Y");
+	}
+
+}
